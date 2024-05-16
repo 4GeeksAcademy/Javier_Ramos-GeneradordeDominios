41b3008c3837a8b9872997c005bfa3f9b2cb84a0
Javier Ramos <162994894+JavierRamos1988@users.noreply.github.com>
2024-05-16 17:26:22 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,25 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let generadordominio = () => {
+  let pronombre = ["el", "nuestro", "este"];
+  let adjetivo = ["increible", "absurdo", "gran", "bizarro"];
+  let sustantivo = ["buscador", "blog", "navegador", "almacen", "consultor"];
+  let extension = [".com", ".es", ".net"];
+
+  let numpronombre = Math.floor(Math.random() * 3);
+  let numadjetivo = Math.floor(Math.random() * 4);
+  let numsustantivo = Math.floor(Math.random() * 5);
+  let numextension = Math.floor(Math.random() * 3);
+
+  return console.log(
+    "www." +
+      pronombre[numpronombre] +
+      adjetivo[numadjetivo] +
+      sustantivo[numsustantivo] +
+      extension[numextension]
+  );
+};
+
+generadordominio();
