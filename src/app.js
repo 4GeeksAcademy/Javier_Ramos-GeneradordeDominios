a413afbab72027232d25fc6b7818fa1bd44454d2
Javier Ramos <162994894+JavierRamos1988@users.noreply.github.com>
2024-05-17 10:50:57 +0000

    generador corregido

--- src/app.js
+++ src/app.js
@@ -11,23 +11,20 @@ window.onload = function() {
 };
 
 let generadorDominio = () => {
-  let pronombre = ["el", "nuestro", "este"];
-  let adjetivo = ["increible", "absurdo", "gran", "bizarro"];
-  let sustantivo = ["buscador", "blog", "navegador", "almacen", "consultor"];
-  let extension = [".com", ".es", ".net"];
+  let pronombres = ["el", "nuestro", "este"];
+  let adjetivos = ["increible", "absurdo", "gran", "bizarro"];
+  let sustantivos = ["buscador", "blog", "navegador", "almacen", "consultor"];
+  let extensiones = [".com", ".es", ".net"];
 
-  let numberPronombre = Math.floor(Math.random() * pronombre.length);
-  let numberAdjetivo = Math.floor(Math.random() * adjetivo.length);
-  let numberSustantivo = Math.floor(Math.random() * sustantivo.length);
-  let numberExtension = Math.floor(Math.random() * extension.length);
-
-  return console.log(
-    "www." +
-      pronombre[numberPronombre] +
-      adjetivo[numberAdjetivo] +
-      sustantivo[numberSustantivo] +
-      extension[numberExtension]
-  );
+  for (let pronombre of pronombres) {
+    for (let adjetivo of adjetivos) {
+      for (let sustantivo of sustantivos) {
+        for (let extension of extensiones) {
+          console.log("www." + pronombre + adjetivo + sustantivo + extension);
+        }
+      }
+    }
+  }
 };
 
 generadorDominio();
