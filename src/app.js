e1b2cf2c0beeb035f2ccf0856fcfc0aa4c63517b
Javier Ramos <162994894+JavierRamos1988@users.noreply.github.com>
2024-05-17 11:37:57 +0000

    ultima actualizacion

--- src/app.js
+++ src/app.js
@@ -5,11 +5,6 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
-
 let generadorDominio = () => {
   let pronombres = ["el", "nuestro", "este"];
   let adjetivos = ["increible", "absurdo", "gran", "bizarro"];
